95b4a6f226dacf37c8d9e9ad7d8513c0cfbd90a4
Christian Elies <crelies@icloud.com>
2020-07-21 22:11:24 +0200

    Initial Commit

---
+++ Shared/TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeApp.swift
+//  Shared
+//
+//  Created by Christian Elies on 21.07.20.
+//
+
+import SwiftUI
+
+@main
+struct TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
